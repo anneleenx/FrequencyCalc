210cfd1affa252c81881d81c5a17f7a9bc0bf8d9
amatushkina <amatushkin@wiley.com>
2018-11-09 17:55:44 +0300

    splash screen (bad version)

--- app/src/main/java/anneleenx/frequencycalc/MainActivity.java
+++ app/src/main/java/anneleenx/frequencycalc/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.CustomTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
